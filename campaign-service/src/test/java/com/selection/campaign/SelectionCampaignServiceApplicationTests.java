8113a73a99b9a67593c06f41e073337213c60437
Suppasek <59070168@it.kmitl.ac.th>
2019-03-19 10:13:54 +0700

    Add test dependencies
    -migrate to Junit5

--- campaign-service/src/test/java/com/selection/campaign/SelectionCampaignServiceApplicationTests.java
+++ campaign-service/src/test/java/com/selection/campaign/SelectionCampaignServiceApplicationTests.java
@@ -1,12 +1,12 @@
 package com.selection.campaign;
 
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class SelectionCampaignServiceApplicationTests {
 
 	@Test
--- campaign-service/src/test/java/com/selection/campaign/repository/CampaignRepositoryTest.java
+++ campaign-service/src/test/java/com/selection/campaign/repository/CampaignRepositoryTest.java
@@ -1,28 +1,33 @@
 package com.selection.campaign.repository;
 
 import com.selection.campaign.model.Campaign;
-import com.selection.campaign.model.CampaignResult;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {CampaignRepository.class})
 class CampaignRepositoryTest {
     @Autowired
     private CampaignRepository campaignRepository = mock(CampaignRepository.class);
 
-    @Before
+    @BeforeEach
     public void setup() {
         campaignRepository.createCampaign();
     }
 
     @Test
     void getAllCampaigns() {
-        ArrayList<Campaign> campaigns = campaignRepository.getAllCampaigns();
+        //ArrayList<Campaign> campaigns = campaignRepository.getAllCampaigns();
         Campaign campaign = campaignRepository.getCampaignById("1");
         assertEquals("Thailand Election", campaign.getName());
         assertEquals("Just a Fair Election", campaign.getDetail());
