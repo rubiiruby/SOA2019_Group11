3ae386c1033b0968079efb2559dc95473e9f6f96
Suppasek <59070168@it.kmitl.ac.th>
2019-04-18 14:48:36 +0700

    Fix package conflict

--- selection-web/src/components/tests/Steps.test.js
+++ selection-web/src/components/tests/Steps.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow, mount, render } from "enzyme";
 import Steps from "../Steps";
+import "../../setupTests";
 
 it("should render without throwing an error", () => {
   const component = shallow(<Steps />);
