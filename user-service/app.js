848cf64c3e1808c168c5700706120fac2d930065
Suppasek <59070168@it.kmitl.ac.th>
2019-04-28 21:00:52 +0700

    Add CORS in user service

--- user-service/app.js
+++ user-service/app.js
@@ -1,12 +1,13 @@
-const express = require("express")
-const app = express()
-const bodyParser = require('body-parser')
-const userController = require('./controllers/userController')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const userController = require("./controllers/userController");
+const cors = require("cors");
 
-var jsonParser = bodyParser.json()
-app.use(urlencodedParser = bodyParser.urlencoded({ extended: false }))
+app.use(cors);
+app.use((urlencodedParser = bodyParser.urlencoded({ extended: false })));
 app.use(bodyParser.json());
 
-app.use('/user', userController);
+app.use("/user", userController);
 
-module.exports = app
+module.exports = app;
