18719e5f5b9dfcb1c15619ec527c55d901de1128
AkeITKMITL <59070083@it.kmitl.ac.th>
2019-05-01 23:18:17 +0700

    update token algorithm

--- user-service/services/jwtService.js
+++ user-service/services/jwtService.js
@@ -3,13 +3,8 @@ const jwt = require("jsonwebtoken")
 const path = require("path")
 
 // use 'utf8' to get string instead of byte array  (512 bit key)
-const privateKEY = fs.readFileSync(
-  path.resolve(__dirname, "../credentials/private.key"),
-  "utf8"
-)
-
-const publicKEY = fs.readFileSync(
-  path.resolve(__dirname, "../credentials/public.key"),
+const secretKey = fs.readFileSync(
+  path.resolve(__dirname, "../credentials/secret.key"),
   "utf8"
 )
 
@@ -21,9 +16,9 @@ module.exports = {
       subject: $Options.subject,
       audience: $Options.audience,
       expiresIn: "30d", // 30 days validity
-      algorithm: "RS256"
+      algorithm: "HS256"
     }
-    return jwt.sign(payload, privateKEY, signOptions)
+    return jwt.sign(payload, secretKey, signOptions)
   },
   verify: (token, $Option) => {
     /*
@@ -38,10 +33,10 @@ module.exports = {
       subject: $Option.subject,
       audience: $Option.audience,
       expiresIn: "30d",
-      algorithm: ["RS256"]
+      algorithm: "HS256"
     }
     try {
-      return jwt.verify(token, publicKEY, verifyOptions)
+      return jwt.verify(token, secretKey, verifyOptions)
     } catch (err) {
       return false
     }
